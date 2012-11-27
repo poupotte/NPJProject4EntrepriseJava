6502304cb1141504cb37c1edb90ca36b145bf7d3
fingolfin <fingolfin@Hithlum>
2012-11-27 15:48:57 +0100

    javadoc + secure init database

--- Converter/src/java/converter/model/CurrencyDTO.java
+++ Converter/src/java/converter/model/CurrencyDTO.java
@@ -5,15 +5,20 @@
 package converter.model;
 
 /**
- *
+ * DTO of the project's Currency
  * @author zoe
  */
 public interface CurrencyDTO {
+
 
     public Float getConversion();
     
     public void setConversion(Float conversion);
  
+    /**
+     * Id of a conversion must be a string
+     * @return 
+     */
     public String getId();
     
     public void setId(String name);
